1e1b7b5d3938015f4f790203ffe7f11944f78b09
Andreagalera <43338979+Andreagalera@users.noreply.github.com>
2018-12-19 17:28:52 +0100

    first commit

---
+++ app/src/main/java/dsa/eetac/upc/edu/finalexamenminimo2/APIRest.java
@@ -0,0 +1,30 @@
+package dsa.eetac.upc.edu.finalexamenminimo2;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public interface APIRest {
+
+    //We specify the url
+    String BASE_URL = "";
+
+    //Get
+    //Post
+    //Pur
+    //Delete
+    static APIRest createAPIRest() {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        return retrofit.create(APIRest.class);
+    }
+}
